12688a2cac66e2e332b5c71739709175a286f4ac
Mateusz <msluszniak@student.agh.edu.pl>
2020-12-15 14:26:23 +0100

    Dziala gridpane

--- src/main/java/agh/cs/project/widget/Main.java
+++ src/main/java/agh/cs/project/widget/Main.java
@@ -7,25 +7,26 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-class Main extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+
         GridPane aPane = new GridPane();
-//        aPane.setPadding(new Insets(10, 10, 10, 10));
-//        aPane.setHgap(1);
-//        aPane.setVgap(1);
-//        for (int row = 1; row <= 6; row++)
-//            for (int col = 1; col <= 8; col++) {
-//                Button b = new Button();
-//                b.setPrefWidth(200);
-//                b.setPrefHeight(200);
-//
-//                if (Math.random() < 0.5)
-//                    b.setStyle("-fx-base: WHITE;");
-//                else
-//                    b.setStyle("-fx-base: BLACK;");
-//                aPane.add(b, col, row);
-//            }
+        aPane.setPadding(new Insets(10, 10, 10, 10));
+        aPane.setHgap(1);
+        aPane.setVgap(1);
+        for (int row = 1; row <= 6; row++)
+            for (int col = 1; col <= 8; col++) {
+                Button b = new Button();
+                b.setPrefWidth(200);
+                b.setPrefHeight(200);
+
+                if (Math.random() < 0.5)
+                    b.setStyle("-fx-base: WHITE;");
+                else
+                    b.setStyle("-fx-base: BLACK;");
+                aPane.add(b, col, row);
+            }
 
 
         primaryStage.setTitle("Simple GridPane Example");
@@ -37,5 +38,7 @@ class Main extends Application {
         launch(args);
     }
 }
+
+
 
 
